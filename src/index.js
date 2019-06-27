d38c8734f3342a7c9106cdc9a8327ef4e85606a8
Alina-Pro <49009597+Alina-Pro@users.noreply.github.com>
2019-06-27 14:44:02 +1000

    Add files via upload

--- src/index.js
+++ src/index.js
@@ -4,6 +4,6 @@ import ReactDOM from "react-dom";
 import "antd/dist/antd.css";
 import "./styles.css";
 
-import ApolloForm from "./components/form";
+import ApolloForm from "./components/form/form";
 
 ReactDOM.render(<ApolloForm />, document.getElementById("root"));
